97058bf86cc4373e9d62b2b7d3e8f178e01a0fbe
Amsu Warner <aswb4@hotmail.com>
2021-06-18 13:55:39 -0400

    Added routes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
 
-// CREATE
+
  // CREATE
 app.post('/users',(req,res)=>{
   User.create(
@@ -29,6 +29,7 @@ app.post('/users',(req,res)=>{
     }
   })
 })
+
 
 
 app.route('/users/:id')
@@ -56,8 +57,37 @@ app.route('/users/:id')
 
 // UPDATE
 .put((req,res)=>{
-  // User.findByIdAndUpdate()
+  User.findByIdAndUpdate(
+    req.params.id,
+    {
+      name:req.body.newData.name,
+      email:req.body.newData.email,
+      password:req.body.newData.password
+    },
+    {
+      new:true
+    },
+    (err,data)=>{
+      if (err){
+        res.json({
+          success: false,
+          message: err
+        })
+      } else if (!data){
+        res.json({
+          success: false,
+          message: "Not Found"
+        })
+      } else {
+        res.json({
+          success: true,
+          data: data
+        })
+      }
+    }
+  )
 })
+
 // DELETE
 .delete((req,res)=>{
   // User.findByIdAndDelete()

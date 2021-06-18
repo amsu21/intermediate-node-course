54f26f9d6e389815db96e40d6ae3cfb781eb8d60
Amsu Warner <aswb4@hotmail.com>
2021-06-17 23:26:13 -0400

    Added database

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+// IMPORT AND CONNECT THE DATABASE
+const User=require('./models/User');
+mongoose.connect('mongodb://localhost/userData')
+
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+module.exports= mongoose.model('User',UserSchema)
